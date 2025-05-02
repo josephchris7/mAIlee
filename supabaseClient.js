16463674273c44ffa052e178c97977321c202db3
josephchris7 <josephchris7777@gmail.com>
2025-05-03 00:09:39 +0530

    Initial commit

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://jvtovirtnnjdttwkwufc.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2dG92aXJ0bm5qZHR0d2t3dWZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxNjgxNTEsImV4cCI6MjA2MTc0NDE1MX0.jGoy5UY5g0i4bmrT7hHiudzLsPXCmWGdojIXB_Ie0a8';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
